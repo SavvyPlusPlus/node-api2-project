5ea3133d80bc75af515b4401aabf251f039246fc
SavvyPlusPlus <info@savvydc.com>
2021-08-08 17:32:37 -0400

    MVP met

--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
+const express = require('express');
 // require your posts router and connect it here
+const postsRouter = require('../api/posts/posts-router.js');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+
+//Other
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>My Project API</h>
+      <p>Welcome to the the API</p>
+    `);
+});
+
+module.exports = server;
